51a2f0beb77aa709ca0276c017380dcb2390881c
subhamwork99 <subhamwork.ojha@gmail.com>
2023-04-20 14:37:11 +0530

    changes in vite config

--- web/frontend/vite.config.js
+++ web/frontend/vite.config.js
@@ -60,4 +60,15 @@ export default defineConfig({
       "^/api(/|(\\?.*)?$)": proxyOptions,
     },
   },
+  build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 });
